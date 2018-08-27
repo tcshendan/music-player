fe38bb77d3bdea09e3d39c17a85590bcdf1900f6
shendan <664542051@qq.com>
2018-08-27 17:58:20 +0800

    fix

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+;
 (function(Vue) {
 
     const serveUrl = `http://localhost:2080/api`
@@ -225,4 +226,4 @@
     // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
     router.start(App, '#app')
 
-})(Vue);
+})(Vue)
