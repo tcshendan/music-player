69621e0031efdb99df9f5047302e732a570d6add
shendan <664542051@qq.com>
2018-08-28 11:07:58 +0800

    增加列表页controller

--- client/app.js
+++ client/app.js
@@ -12,6 +12,10 @@
                 controller: 'HomeController',
                 templateUrl: 'home'
             })
+            .when('/list', {
+                controller: 'ListController',
+                templateUrl: 'list'
+            })
             .otherwise({
                 redirectTo: '/home'
             })
@@ -21,4 +25,43 @@
     app.controller('HomeController', ['$scope', function($scope) {
 
     }]);
+
+    //列表页Controller
+    app.controller('ListController', ['$scope', '$http', function($scope, $http) {
+        //数据初始化（计算、ajax）操作（$http）
+        $scope.data = {};
+
+        //数据初始化（计算、ajax）操作（$http）
+        $http.jsonp('http://localhost:2080/api/music?callback=JSON_CALLBACK')
+            .then(res => {
+                console.log(res);
+                $scope.data.list = res.data;
+
+            });
+
+        //行为操作初始化
+        $scope.action = {};
+    }]);
+
+    app.service('padService', function() {
+        this.pad = function(num, n) {
+            return (Array(n).join(0) + num).slice(-n);
+        }
+    });
+
+    app.filter('covertNum', ['padService', function(padService) {
+        return function(num, n) {
+            return padService.pad(num, n);
+        }
+    }]);
+
+    app.filter('convertDuration', ['padService', function(padService) {
+        return function(duration) {
+            var h = Math.floor(duration / 3600);
+            var m = Math.floor(duration % 3600 / 60);
+            var s = Math.floor(duration % 60);
+            return h ? padService.pad(h, 2) + ':' + padService.pad(m, 2) + ':' + padService.pad(s, 2) : padService.pad(m, 2) + ':' + padService.pad(s, 2);
+        }
+    }]);
+
 })(angular)
